c8b56face32639095fb0d6bc53bdac8fdaf1cd16
Chun Jie Chong <96840020+Chun-Jie-Chong@users.noreply.github.com>
2024-04-11 17:29:16 -0400

    Add JAVA files

---
+++ Java/src/main/java/com/graph/Graph.java
@@ -0,0 +1,44 @@
+package com.graph;
+
+import java.util.*;
+
+class Graph {
+    private int V;
+    private List<List<Integer>> adj;
+
+    public Graph(int V) {
+        this.V = V;
+        adj = new ArrayList<>(V);
+        for (int i = 0; i < V; i++) {
+            adj.add(new LinkedList<>());
+        }
+    }
+
+    public void addEdge(int v, int w) {
+        adj.get(v).add(w);
+        adj.get(w).add(v);
+    }
+
+    public void printGraph() {
+        for (int v = 0; v < V; v++) {
+            System.out.print("Adjacency list of vertex " + v + ": ");
+            for (Integer neighbor : adj.get(v)) {
+                System.out.print(neighbor + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(5);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+
+        graph.printGraph();
+    }
+}
